260b4ed7115c64b6ee0cb260a19014f7e0d4291e
adamhaik03 <107291787+adamhaik03@users.noreply.github.com>
2022-06-17 17:46:37 +0100

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/function1.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+    var String = "Haha aa tu jest twoja pomarancza skurwysynu!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML =
+    result;
+}
